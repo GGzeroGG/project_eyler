80d394ec58d702321a03faa3154440e22b9fafd2
dmitry <flesh8228@gmail.com>
2020-09-18 15:51:44 +0300

    * Рещенная задачи 3

--- tasks/3/main.py
+++ tasks/3/main.py
@@ -1,12 +1,26 @@
 number = 600851475143
 divisor_max = 0
 
-while number == 1:
-    for supposed_divisor in (2, number + 1):
+while number > 1:
+    """
+    Цикл который идет пока число не дойдет до 1
+    """
+    for supposed_divisor in range(2, number + 1):
+        """
+        Цикл который выдает предпологаемый делитель
+        """
         if (number % supposed_divisor) == 0:
+            """
+            Проверяем если число делится без остатка значит это наш делитель
+            """
+            # Делим число на простой делитель чтобы разкладывать его дальше
+            number //= supposed_divisor
             if divisor_max < supposed_divisor:
+                """
+                Если наш делитель больше текущего максимального то меняем их 
+                местами
+                """
                 divisor_max = supposed_divisor
-            number = number / supposed_divisor
             break
 
 print(divisor_max)
