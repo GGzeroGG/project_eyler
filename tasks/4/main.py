7662c03fd0e1db280eec0b229b993eee02b07f97
dmitry <flesh8228@gmail.com>
2020-09-23 14:56:56 +0300

    * Рещенная задачи 4

--- tasks/4/main.py
+++ tasks/4/main.py
@@ -1,9 +1,20 @@
 maximum_palindrome = 0
 
-for factor_1 in range(100, 999 + 1):
-    for factor_2 in range(100, 999 + 1):
-        composition = factor_1 * factor_2
+for multiplied in range(100, 999 + 1):
+    """
+    Берем все 3-х значные числа для умножаемого
+    """
+    for factor in range(multiplied, 999 + 1):
+        """
+        Берем все 3-х значные множители исключая те что мы уже умножали
+        """
+        # Умножаем числа получем произведение
+        composition = multiplied * factor
         if str(composition) == str(composition)[::-1] and composition > maximum_palindrome:
+            """
+            Если произведение полиндром и оно болье текущего самого большого
+            полиндрома то меняим их местами
+            """
             maximum_palindrome = composition
 
 print(maximum_palindrome)
---
+++ tasks/5/main.py
@@ -0,0 +1,4 @@
+number = 1
+estimated_number = 20
+
+
