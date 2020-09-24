d45bfcd9bb920e21187c30a2662a97b440ecd5ec
dmitry <flesh8228@gmail.com>
2020-09-24 15:15:52 +0300

    * Рещенная задачи 4

--- tasks/1/main.py
+++ tasks/1/main.py
@@ -8,3 +8,35 @@ for number in range(1, 1000):
         amount += number
 
 print(amount)
+
+
+def multiples_of_range(number_start, number_end, multiples):
+    """
+    Вычисляет все числа кртные multiples диапазона number_start-number_end
+    number_start - начало диапазона, включительно: число
+    number_end - конец диапазона, включительно: число
+    multiples - числа которым надо найти кратное: кортеж
+    """
+
+    numbers = []
+    len_multiples = len(multiples)
+
+    for number in range(number_start, number_end + 1):
+        audits_carried_out = 0
+
+        for multiple in multiples:
+            if multiple % number == 0:
+                audits_carried_out += 1
+            else:
+                break
+
+        if audits_carried_out == len_multiples:
+            numbers.append(number)
+
+    return numbers
+
+
+n = multiples_of_range(1, 10, (3, 5))
+print(n)
+
+
